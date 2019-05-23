bb03de2007ad6cd4a0786bed0e3b4ef444c7170a
Alex Aguilar <Alex.Aguilar@washpost.com>
2019-05-23 23:37:40 +0200

    add output type

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -1,28 +1,24 @@
-'use strict'
-
+/* React */
 import React from 'react'
 
-export default ({
-  children,
-  contextPath,
-  deployment,
-  CssLinks,
-  Fusion,
-  Libs,
-  MetaTags
-}) =>
-  <html>
-    <head>
-      <title>Fusion Article</title>
-      <MetaTags />
-      <Libs />
-      <CssLinks />
-      <link rel='icon' type='image/x-icon' href={deployment(`${contextPath}/resources/favicon.ico`)} />
-    </head>
-    <body>
-      <div id='fusion-app'>
-        {children}
-      </div>
-      <Fusion />
-    </body>
-  </html>
+export default (props) => {
+  return (
+    <html>
+      <head>
+        <title>{props.metaValue('title') || 'Default Title'}</title>
+        <props.MetaTags />
+        <props.Libs />
+        <props.CssLinks />
+        <link rel='icon' type='image/x-icon' href={props.deployment(`${props.contextPath}/resources/img/favicon.ico`)} />
+        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' />
+      </head>
+      <body>
+        <h1>Welcome to Fusion!</h1>
+        <div id='fusion-app' className='col-12'>
+          {props.children}
+        </div>
+        <props.Fusion />
+      </body>
+    </html>
+  )
+}
